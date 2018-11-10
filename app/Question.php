1d457f3d18ba7a1043632c9b88ab5e3e358a9916
alexn <anazarenco@best4u.md>
2018-11-10 12:54:38 +0200

    Add votes answer and views countes to question item

--- app/Question.php
+++ app/Question.php
@@ -48,4 +48,18 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    /**
+     * @return string
+     */
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
