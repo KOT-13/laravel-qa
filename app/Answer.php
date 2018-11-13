3d7ebf7a602c07a22608fc68f4b23ea847f647f3
alexn <anazarenco@best4u.md>
2018-11-13 12:09:10 +0200

    Update best_answer_id on questions table when answer removed via new migration with foreign SETT NULL

--- app/Answer.php
+++ app/Answer.php
@@ -46,12 +46,7 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if ($question->best_answer_id === $answer->id) {
-                $question->best_answer_id = NULL;
-                $question->save();
-            }
+            $answer->question->decrement('answers_count');
         });
     }
 
@@ -63,6 +58,9 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    /**
+     * @return string
+     */
     public function getStatusAttribute()
     {
         return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
